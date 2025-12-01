27ee72102516793574e8163be21ea3ec46dc506c
Zillur-Rahman-Abdullah <zrabdullaho1@gmail.com>
2025-12-01 16:36:57 +0600

    commit

--- app.js
+++ app.js
@@ -1,17 +1,39 @@
+ 
 const express = require('express')
+var cors = require('cors')
 const app = express()
-const port = 3000
-
+ 
+var dynamicCorsOptions = function (req, callback) {
+  var corsOptions;
+  if (req.path.startsWith('/')) {
+    corsOptions = {
+      origin: 'http://localhost:3000', // Allow only a specific origin
+      credentials: true,            // Enable cookies and credentials
+    };
+  } else {
+    corsOptions = { origin: '*' };   // Allow all origins for other routes
+  }
+  callback(null, corsOptions);
+};
+ 
+app.use(cors(dynamicCorsOptions));
+ 
+const port = 4000
 const userRoutes = require('./routes/users')
-
-app.use(express.json())
-
-app.use('/users', userRoutes)
-
+ 
+// JSON parser MUST be before routes
+app.use(express.json());
+ 
 app.get('/', (req, res) => {
-  res.send('Hello 2020 khhjhjhj')
+  res.send('Hello World! Naimur')
 })
-
+ 
+app.use('/user', require('./routes/users'));
+ 
+app.use('/static', express.static('public'));
+ 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+ 
+ 
